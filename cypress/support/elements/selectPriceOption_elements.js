6cb83dce774099c883a27f3aacd093f41948f381
leonylopes <leonylopes@users.noreply.github.com>
2025-05-12 19:57:20 +0000

    deploy: a793ad7e5b351e7aa924d41b0224a90c7f4f6709

--- cypress/support/elements/selectPriceOption_elements.js
+++
@@ -1,6 +0,0 @@
-class selectOption_elements {
-    priceOptionField = () => { return '#selectgold' };
-    nextButton = () => { return '#nextsendquote'};
-};
-
-export default selectOption_elements;
