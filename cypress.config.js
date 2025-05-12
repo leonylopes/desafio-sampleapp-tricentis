96274705b0040a42a637b0ecd830b590f3a02555
leonylopes <leony.1710@gmail.com>
2025-05-12 16:48:42 -0300

    configurando allure report

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,14 @@
 const { defineConfig } = require("cypress");
 const cucumber = require('cypress-cucumber-preprocessor').default
+const { allureCypress } = require("allure-cypress/reporter");
 
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      on('file:preprocessor', cucumber())
+      on('file:preprocessor', cucumber());
+      allureCypress(on, config);
+      return config;
     },
     baseUrlAutomationInsurance: 'https://sampleapp.tricentis.com/101/app.php',
     specPattern: "**/*.feature",
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,6 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import 'allure-cypress'
 
 
 // Alternatively you can use CommonJS syntax:
